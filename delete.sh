bf3a1b3a83cbb97dcfa343e5ad22148eae974fd8
Aditya R. Jawanjal <97746325+AdiJawanjal@users.noreply.github.com>
2025-10-23 14:37:26 +0530

    Create delete.sh for file deletion
    Add a script to delete multiple files based on user input.

---
+++ delete.sh
@@ -0,0 +1,9 @@
+read -p "Enter no. of files you want to delete:" files
+
+Count=1
+
+while [ $Count -le $files ]; do
+        read -p "Enter the Name of the file:" name
+        rm "$name"
+        Count=$((Count + 1))
+done
